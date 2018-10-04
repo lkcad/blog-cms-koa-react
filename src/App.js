82bbe49c13714ffad12a9e48e2623729e9d62c46
Good__luck <2924456233@qq.com>
2018-10-04 14:36:37 +0800

    add scss suport

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Button} from 'antd';
 import './App.css';
+import style from './example.scss'
 
 class App extends Component {
+    componentDidMount() {
+
+    }
+
+
     render() {
         return (
             <div className="App">
+                <div className={style.qqq}>import scss</div>
                 <Button type="primary">Button</Button>
             </div>
         );
